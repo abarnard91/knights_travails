1627a5034c16882b4f86f54bb5d91ba15b998355
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2024-10-22 19:51:38 +0000

    solved the major problem with the traversal methdo and the project removed commented out debugging Lines added comment notes to explain the methods added 3 examples on main.rb

--- lib/graph.rb
+++ lib/graph.rb
@@ -6,26 +6,24 @@ class Graph
     @move_options_arr = [[2,1],[-2,1],[2,-1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
   end
 
-  def create_vl
+  def create_vl #creates a list of all possible locations (coordinates) on the chess board
     x_axis = 0 
     y_axis = 0
-    vertex_list = Array.new
     while x_axis < 8
       while y_axis < 8
         vertex = Node.new([x_axis, y_axis])
-        vertex_list << vertex
+        @vertex_list << vertex
         y_axis += 1
       end 
       x_axis += 1
       y_axis = 0
     end
-    @vertex_list = vertex_list
     return @vertex_list
   end
 
-  def knight_move(input, move)
+  def knight_move(input, move) #creates new coordinates within the range of 0-7 so the night doesnt go off the board
     coordinates = Array.new
-    if ((input[0] + move[0]) >= 0) && (input[0] + move[0] < 8)
+    if ((input[0] + move[0]) >= 0) && (input[0] + move[0] < 8) 
         if ((input[1] + move[1]) >= 0) && (input[1] + move[1] < 8)
             coordinates << (input[0] + move[0])
             coordinates << (input[1] + move[1])
@@ -51,74 +49,123 @@ class Graph
         end
       end
     end
-    #moves
+  end
+
+  def c_2_n(c) #coordinates to node
+    @vertex_list.each do |v|
+      if v.data == c
+        c = v
+        break
+      end
+    end
+    c = "Coordinates not found" unless c.is_a?(Node)
+    return c
   end
 
   def traversal(start_place, end_place)
-    sc = nil #starting coordinates
-    visited_verticies = Array.new
+    sc = self.c_2_n(start_place) #starting coordinates in node class
+    ec = self.c_2_n(end_place) #ending coordinates in node class
+    
+    visited_vertices = Array.new
     moves = Array.new 
     queue = Array.new
-    loms = Array.new #list of move sets
-    @vertex_list.each do |v|
-      if start_place == v.data
-        sc = v
-        visited_verticies.push(sc)
-        queue.push(sc)
-        moves.push(sc)
-      end
-    end
-    if sc.nil?
-        return "Starting coordinates not found"
-    end
-
-    until queue.empty? 
-    #10.times do
-      #puts queue.length
-      queue.each do |move|
-        #puts move
-        visited_verticies << move unless visited_verticies.include?(move)
-        moves << move unless moves.include?(move)
-        if move.data == end_place
-          moves.map!{|vertex| vertex.data}
-          loms << moves unless loms.include?(moves)
-          moves = [sc]
+    parent_vertex = nil
+    
+    until sc == ec #loop of bft of graph 
+      queue << sc
+      queue.each do |vertex|#iterate through vertices
+        visited_vertices << vertex unless visited_vertices.include?(vertex) #add to visited vertices list so infinite loops of the same vertex/edges arent produced
+        parent_vertex = vertex #to store the vertex of the edge that is the end coordinate
+        if vertex == ec # ie if the end place is one move or less away
+          moves.unshift(vertex) 
           break
-          puts "Why are you seeing this!"
         else
-          #puts "HIIIIII"
-          move.edges.each do |edge|
-            if edge.data == end_place
-              moves << edge 
-              moves.map!{|vertex| vertex.data}
-              loms << moves unless loms.include?(moves)
-              moves = [sc]
+          vertex.edges.each do |edge| #iterate through the edges of the current vertex in the queue
+            if edge == ec #if the current vertex contains an edge to the endplace vertext
+              moves.unshift(edge) #add edge to the beginning of moves list
               break
-              puts "You shouldn\'t see this"
             else
-              if queue.include?(edge) || visited_verticies.include?(edge) 
-                #puts "its already there"
-              else
-                queue << edge
-                #puts "added"
-              end
+              queue << edge unless visited_vertices.include?(edge) #otherwise add edges to the queue to iterate over as the next vertex
             end
           end
+          parent_vertex = nil unless moves[0] == ec #if none of the edges connect to the end space vertex forget the current vertex and assign it to the next vertex in the queue
+        end
+        if moves[0] == ec #if the edge of the vertex is the end place coordinate
+          ec = parent_vertex #end place node becomes the parent vertex for the next round 
+          parent_vertex = nil# and everything is reset. this builds the path backwards and eventually breaks the while(until) loop
+          queue = []
+          visited_vertices = []
+          break   
         end
       end
-      queue.shift
     end
-    spl = 65 #shortest path length
-    spi = 0
-   loms.each_with_index do |move_set, i|
-    puts "#{i} #{move_set}"
-
-    if move_set.length < spl
-      spl = move_set.length 
-      spi = i #shortest path index
-    end
-   end
-   puts "#{spl} #{loms[spi]}"
+    moves.unshift(sc) #adds the starting coordinate to the moves list as the first "move"
+    moves.map!(&:data) #pulls out just the coordinates from the moves list for easier reading
+    puts "path from #{start_place} to #{end_place}"
+    puts "number of moves = #{moves.length-1} \nmoves: #{moves}"
   end
-  
 end
+
+
+
+
+
+
+
+
+
+#queue.each do |move|
+  #     visited_verticies << move unless visited_verticies.include?(move)
+  #     moves << move unless moves.include?(move)
+  #     if move.data == end_place
+  #       puts "hi"
+  #       sc.edges.each do |e|
+  #         puts "moves[1] is #{moves[1].data} and e is #{e.data}"
+  #         if moves[1] == e
+  #           moves.map!{|vertex| vertex.data}
+  #           loms << moves unless loms.include?(moves)
+  #         end
+  #       end
+  #       moves = [sc]
+  #       break
+  #       puts "Why are you seeing this!"
+  #     else
+  #       move.edges.each do |edge|
+  #         if edge.data == end_place
+  #           #puts "from edges"
+  #           moves << edge 
+  #           sc.edges.each do |e|
+  #             puts "moves[1] is #{moves[1].to_s} and e is #{e.data}"
+  #             if moves[1] == e
+  #               moves.map!{|vertex| vertex.data}
+  #               loms << moves unless loms.include?(moves)
+  #             end
+  #           end
+  #           # moves.map!{|vertex| vertex.data}
+  #           # loms << moves unless loms.include?(moves)
+  #           moves = [sc]
+  #           break
+  #           puts "You shouldn\'t see this"
+  #         else
+  #           if queue.include?(edge) || visited_verticies.include?(edge) 
+  #             #puts "its already there"
+  #           else
+  #             queue << edge
+  #             #puts "added #{edge.data}"
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  #   queue.shift
+  # end
+
+  #spl = 65 #shortest path length starts at 65 b/c it cant be bigger than that without retreading on spaces
+  #   spi = 0 #shortest path index
+  #  moves.each_with_index do |move_set, i|
+  #   #puts "#{i} #{move_set}"
+
+  #   if move_set.length < spl
+  #     spl = move_set.length 
+  #     spi = i 
+  #   end
--- main.rb
+++ main.rb
@@ -5,30 +5,10 @@ test = Graph.new
 
 test.create_vl
 
-#test.vertex_list.length.times do |i|
-#    puts "#{test.vertex_list[i].data}"
-#end
-
 test.vertex_list.each do |v|
   test.move_options(v.data)
 end
 
-# test.vertex_list.each do |vertex|
-#   puts "vertex:"
-#   vertex.to_s
-#   vertex.edges.each do |edge|
-#     puts "edge:"
-#     edge.to_s
-#   end
-#   puts "break"
-# end
-#test.move_options(test.vertex_list[2].data)
-#test.vertex_list[27].edges.each do |i|
-  #puts "vertex:#{test.vertex_list[27].data} edge:#{i.data}"
-  #puts i.edges
-#end
-#
-
-#test.traversal([3,3], [7,2])
-#test.traversal([0,0],[1,2])
+test.traversal([3,3], [0,0])
+test.traversal([0,0],[1,2])
 test.traversal([0,0], [7,7])
